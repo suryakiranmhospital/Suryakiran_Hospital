73ccbd4c3dabac18690394647c7d97389c785805
SKMH <suryakiran.m.hospital@gmail.com>
2026-04-15 10:17:19 +0530

    Update Doctors.tsx

--- components/Doctors.tsx
+++ components/Doctors.tsx
@@ -13,7 +13,7 @@ const doctors = [
     degree: "M.S. (Gen. Surgery)",
     descriptionKey: "drMahimaDesc",
     timingKey: "drMahimaTiming",
-    image: "/images/mmahima.jpg",
+    image: "/images/mahima.jpg",
   },
   {
     nameKey: "drAbhishiek",
